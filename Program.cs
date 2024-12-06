b81c17a7457642afadb9d66bcf6bbc2174cbd880
M4KS1m29 <ostrovskyi_ak24@nuwm.edu.ua>
2024-12-06 22:49:56 +0200

    покращення

--- Program.cs
+++ Program.cs
@@ -14,15 +14,23 @@ public class GraphForm : Form
         this.Paint += DrawGraph;
     }
 
+    // Обчислення значення функції
+    private float CalculateY(float x)
+    {
+        // Обробка крайового випадку для x = 0
+        if (x == 0) return float.NaN; // Повертаємо NaN для x = 0
+        return (float)(Math.Pow(Math.Cos(x), 2) / (Math.Pow(x, 2) + 1));
+    }
+
     private void DrawGraph(object sender, PaintEventArgs e)
     {
         Graphics g = e.Graphics;
         g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
         // Встановлення параметрів графіка
-        float xMin = 3.8f;
-        float xMax = 7.6f;
-        float step = 0.6f;
+        float xStart = 3.8f; // Початкове значення x
+        float xEnd = 7.6f;   // Кінцеве значення x
+        float step = 0.6f;   // Крок для розрахунку функції
 
         // Розміри форми
         int width = this.ClientSize.Width;
@@ -40,36 +48,48 @@ public class GraphForm : Form
         g.DrawLine(axisPen, graphArea.Left + graphArea.Width / 2, graphArea.Top, graphArea.Left + graphArea.Width / 2, graphArea.Bottom); // Y-вісь
 
         // Функція для масштабування координат
-        Func<float, float, float, float> scaleX = (x, xMinVal, xMaxVal) => graphArea.Left + (x - xMinVal) / (xMaxVal - xMinVal) * graphArea.Width;
-        Func<float, float, float, float> scaleY = (y, yMinVal, yMaxVal) => graphArea.Bottom - (y - yMinVal) / (yMaxVal - yMinVal) * graphArea.Height;
+        Func<float, float, float, float> scaleX = (x, xMinVal, xMaxVal) =>
+            graphArea.Left + (x - xMinVal) / (xMaxVal - xMinVal) * graphArea.Width;
+
+        Func<float, float, float, float> scaleY = (y, yMinVal, yMaxVal) =>
+            graphArea.Bottom - (y - yMinVal) / (yMaxVal - yMinVal) * graphArea.Height;
 
         // Визначення мінімальних і максимальних значень функції для масштабування
         float yMin = float.MaxValue;
         float yMax = float.MinValue;
-        for (float x = xMin; x <= xMax; x += step)
+
+        for (float x = xStart; x <= xEnd; x += step)
         {
-            float y = (float)(Math.Pow(Math.Cos(x), 2) / (Math.Pow(x, 2) + 1));
-            if (y < yMin) yMin = y;
-            if (y > yMax) yMax = y;
+            float y = CalculateY(x);
+            if (!float.IsNaN(y))
+            {
+                if (y < yMin) yMin = y;
+                if (y > yMax) yMax = y;
+            }
         }
 
         // Малювання графіка
         Pen graphPen = new Pen(Color.Blue, 2);
         PointF? prevPoint = null;
-        for (float x = xMin; x <= xMax; x += step)
-        {
-            float y = (float)(Math.Pow(Math.Cos(x), 2) / (Math.Pow(x, 2) + 1));
-            float scaledX = scaleX(x, xMin, xMax);
-            float scaledY = scaleY(y, yMin, yMax);
 
-            PointF currentPoint = new PointF(scaledX, scaledY);
-            if (prevPoint != null)
+        for (float x = xStart; x <= xEnd; x += step)
+        {
+            float y = CalculateY(x);
+            if (!float.IsNaN(y))
             {
-                g.DrawLine(graphPen, prevPoint.Value, currentPoint);
+                float scaledX = scaleX(x, xStart, xEnd);
+                float scaledY = scaleY(y, yMin, yMax);
+
+                PointF currentPoint = new PointF(scaledX, scaledY);
+                if (prevPoint != null)
+                {
+                    g.DrawLine(graphPen, prevPoint.Value, currentPoint);
+                }
+                prevPoint = currentPoint;
             }
-            prevPoint = currentPoint;
         }
     }
+
     [STAThread]
     public static void Main()
     {
